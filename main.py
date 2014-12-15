fdcf135b123ad37763abf4808cf4c22f0d7de351
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-12-15 23:50:29 +0100

    1) Created Application class, refactored 2) Changed window bar title

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # December 12 2014
 #
 
-# TODO | -
+# TODO | - App class (?)
 #        -
 #
 # SPEC | -
@@ -14,7 +14,6 @@
 
 
 
-from inspect import getouterframes, currentframe, getframeinfo
 from collections import namedtuple
 
 import pygame
@@ -22,21 +21,12 @@ from pygame.locals import *
 from SwiftUtils.EventDispatcher import EventDispatcher
 from SwiftUtils.MultiSwitch import MultiSwitch
 
-from math import sin, cos, pi as π
+from math import sin, cos, e, pi as π
 
 from Piano import Piano
+from Chords import Chords
+from utilities import debug
 
-
-DEBUG = False
-
-if not DEBUG:
-	debug = lambda *args, **kwargs: None
-else:
-	def debug(*args, **kwargs):
-		# Dangerous hack
-		caller = getframeinfo(currentframe().f_back)
-		print('[{0.lineno}]'.format(caller), end=' ')
-		print(*args, **kwargs)
 
 
 Context = namedtuple('Context', 'surface size events clock')
@@ -55,6 +45,72 @@ colours = {
 }
 
 
+
+class Application(EventDispatcher):
+
+	'''
+	Docstring goes here
+
+	'''
+
+	def __init__(self, title, size, resizable=True):
+		
+		'''
+		Docstring goes here
+
+		'''
+
+		# Initialize basics
+		pygame.init()
+		self.canvas = pygame.display.set_mode(size)
+		pygame.display.set_caption(title)
+		self.clock = pygame.time.Clock()
+
+		# App data
+		self.piano = Piano()
+
+		# Events
+		self.bindEvents()
+
+
+	def bindEvents(self):
+
+		'''
+		Docstring goes here
+
+		'''
+		
+		self.always = lambda dt: self.tick(dt)
+		self.bind({'type': KEYDOWN, 'mod': 1}, lambda p, e: print('Hello'))
+		self.bind({'type': KEYDOWN, 'key': K_ESCAPE}, lambda p, e: pygame.quit())
+		self.bind({'type': KEYDOWN, 'key': K_SPACE, 'doc': 'Play an F chord'}, lambda p, e: (print(p.doc), self.piano.playChord((5, 9, 12))))
+		self.bind({'type': KEYUP, 'key': K_SPACE}, lambda p, e: self.piano.playChord((5, 9, 12), fill=(255, 255, 255)))
+
+
+	def tick(self):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		self.canvas.fill(((0, 72, 50), (0xFF, 0xFF, 0), colours['BG'])[2])
+		self.piano.render(surface, (20, 20))
+		pygame.display.flip()
+		self.clock.tick(60)
+
+
+	def run(self):
+
+		'''
+		Docstring goes here
+
+		'''
+
+		self.mainloop()
+
+
+
 def createContext(size):
 
 	'''
@@ -64,6 +120,7 @@ def createContext(size):
 
 	pygame.init()
 	surface = pygame.display.set_mode(size)
+	pygame.display.set_caption('Chordially')
 	clock = pygame.time.Clock()
 
 	return Context(surface, size, EventDispatcher(), clock)
@@ -99,21 +156,23 @@ def main():
 	'''
 
 	# Initialize basic components (pygame, events, window)
-	ctx = createContext((720, 480))
+	# ctx = createContext((720*2, 480))
 	
-	# Setup
-	mFont = pygame.font.SysFont('oldenglishtext', 20)
-	world = namedtuple('Word', 'piano')(Piano())
+	# # Setup
+	# mFont = pygame.font.SysFont('oldenglishtext', 20)
+	# world = namedtuple('Word', 'piano')(Piano())
 
-	# Events
-	ctx.events.always = lambda dt: tick(dt, ctx, world)
-	ctx.events.bind({'type': KEYDOWN, 'mod': 1}, lambda p, e: print('Hello'))
-	ctx.events.bind({'type': KEYDOWN, 'key': K_ESCAPE}, lambda p, e: pygame.quit())
-	ctx.events.bind({'type': KEYDOWN, 'key': K_SPACE, 'doc': 'Play an F chord'}, lambda p, e: (print(p.doc), world.piano.playChord((5, 9, 12))))
-	ctx.events.bind({'type': KEYUP, 'key': K_SPACE}, lambda p, e: world.piano.playChord((5, 9, 12), fill=(255, 255, 255)))
+	# # Events
+	# ctx.events.always = lambda dt: tick(dt, ctx, world)
+	# ctx.events.bind({'type': KEYDOWN, 'mod': 1}, lambda p, e: print('Hello'))
+	# ctx.events.bind({'type': KEYDOWN, 'key': K_ESCAPE}, lambda p, e: pygame.quit())
+	# ctx.events.bind({'type': KEYDOWN, 'key': K_SPACE, 'doc': 'Play an F chord'}, lambda p, e: (print(p.doc), world.piano.playChord((5, 9, 12))))
+	# ctx.events.bind({'type': KEYUP, 'key': K_SPACE}, lambda p, e: world.piano.playChord((5, 9, 12), fill=(255, 255, 255)))
 
-	ctx.events.mainloop()
+	# ctx.events.mainloop()
 
+	app = Application('Chordially', (720*2, 480))
+	app.run()
 
 
 if __name__ == '__main__':
